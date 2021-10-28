ca6817da6458c365b1043d2c2b4103ddf9f5c9fd
Austin Hunt <32047552+hausteen@users.noreply.github.com>
2021-10-28 17:27:46 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Changing ownership of the files"
+chown root:root update.sh
+chown root:root update.service
+chown root:root update.timer
+echo "Setting permissions on the files"
+chmod 700 update.sh
+chmod 700 update.service
+chmod 700 update.timer
+echo "Moving files to correct folder locations"
+mv update.sh /root/
+mv update.service /etc/systemd/system/
+mv update.timer /etc/systemd/system/
+echo "Enabling the update.timer unit file on startup"
+systemctl enable update.timer
+echo "Reloading the systemctl daemon"
+systemctl daemon-reload
